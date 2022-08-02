789aceac7366e02ae634337079d52e3021dbad3a
Zireael <1193875164@qq.com>
2022-08-02 11:39:54 +0800

    request func kansei

---
+++ src/components/MainPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { request } from "@/request/index.js";
+
+export default {
+  mounted() {
+    // request({
+    //   url: "/student/testToken",
+    //   method: "POST",
+    // })
+    // .then(res =>{
+    //     console.log(res);
+    // },err =>{
+    //     console.log(err);
+    // })
+  },
+};
+</script>
+
+<style>
+</style>
