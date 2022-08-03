eb5bd2161506cea5a4a6528d7b4bb64e5104319d
HACK-0123-SYR <78593366+HACK-0123-SYR@users.noreply.github.com>
2022-08-03 18:07:01 +0800

    h

--- src/request/index.js
+++ src/request/index.js
@@ -1,43 +1,45 @@
 import axios from "axios";
-import nprogress from 'nprogress'
-import 'nprogress/nprogress.css'
+import nprogress from "nprogress";
+import "nprogress/nprogress.css";
 
 // 发送登录请求，用于接收用户信息
 // 定义好后，在登陆页面使用
 const login = (data) => {
-    return request({
-        url: '/lg/login',
-        method: 'POST',
-        data
-    })
-}
+  return request({
+    url: "/lg/login",
+    method: "POST",
+    data,
+  });
+};
+
+//jguyg
 
 const request = axios.create({
-    baseURL: 'http://150.158.94.140:3658',
-    // baseURL: 'http://127.0.0.1:3658',
-    timeout: 50000,
-})
+  baseURL: "http://150.158.94.140:3658",
+  // baseURL: 'http://127.0.0.1:3658',
+  timeout: 50000,
+});
 
-request.interceptors.request.use(config => {
-    config.headers["Authorization"] = `Bearer ${localStorage.getItem('token')}`
-    nprogress.start()
-    return config
-})
+request.interceptors.request.use((config) => {
+  config.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
+  nprogress.start();
+  return config;
+});
 
-request.interceptors.response.use(res => {
-    nprogress.done()
-    return res
-}, err => {
+request.interceptors.response.use(
+  (res) => {
+    nprogress.done();
+    return res;
+  },
+  (err) => {
     console.log(err);
     return {
-        data: {
-            msg: err.message,
-            code: 500
-        }
-    }
-})
+      data: {
+        msg: err.message,
+        code: 500,
+      },
+    };
+  }
+);
 
-export {
-    request,
-    
-}
+export { request };
