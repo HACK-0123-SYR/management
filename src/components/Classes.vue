715379fed91635eafbb6d0d1970147ce09f8877d
HACK-0123-SYR <78593366+HACK-0123-SYR@users.noreply.github.com>
2022-08-07 18:02:32 +0800

    班级管理2

--- src/components/Classes.vue
+++ src/components/Classes.vue
@@ -1,29 +1,68 @@
 <template>
   <div class="main">
-    <el-menu
-      :default-active="activeIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-    >
-      <el-menu-item index="1">banji</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-          <el-menu-item index="2-4-2">选项2</el-menu-item>
-          <el-menu-item index="2-4-3">选项3</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="3" disabled>消息中心</el-menu-item>
-      <el-menu-item index="4"
-        ><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item
-      >
-    </el-menu>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="我的班级" name="first" >
+          <el-button type="primary" plain style="margin-left: 16px;">新建班级</el-button>
+        <div class="style_classListBox__NBMYd">
+          <div class="style_listBox__40SsE">
+
+
+            <div class="style_item__BJ73k">
+              <div class="style_top__09yUH">
+                <div class="style_left__l5taP">
+                  <img
+                    src="https://fps.yangcongxueyuan.com/static/media/class0.304ad61c98fff5d19f50.png"
+                    width="100%"
+                    alt=""
+                  />
+                </div>
+                <div class="style_right__Oo-vb">
+                  <div class="style_name__TgRjJ">907203</div>
+                  <div class="style_num__PzqFD">
+                    <span>班内暂无学生</span>
+                  </div>
+                </div>
+              </div>
+              <div class="style_btm__xPuHV">
+                <el-button type="primary" icon="el-icon-edit" round plain style="margin-top:20px">班级管理</el-button>
+              </div>
+            </div>
+
+
+            <div class="style_item__BJ73k">
+              <div class="style_top__09yUH">
+                <div class="style_left__l5taP">
+                  <img
+                    src="https://fps.yangcongxueyuan.com/static/media/class1.6ab38f5420877c66d50b.png"
+                    width="100%"
+                    alt=""
+                  />
+                </div>
+                <div class="style_right__Oo-vb">
+                  <div class="style_name__TgRjJ">906675</div>
+                  <div class="style_num__PzqFD">
+                    <ul>
+                      <li>班级编号：906675</li>
+                      <li>老师：1</li>
+                      <li>学生：4</li>
+                      <li>家长：0</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="style_btm__xPuHV">
+                <el-button type="primary" icon="el-icon-edit" round plain style="margin-top:20px">班级管理</el-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </el-tab-pane>
+
+      <el-tab-pane label="班级学情" name="second">
+        班级学情
+        
+        </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <style>
@@ -48,16 +87,6 @@ export default {
     ...mapState("classMap"),
   },
 };
-
-// import { reactive, toRefs } from 'vue'
-
-// const state = reactive({
-//   circleUrl:
-//     'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
-//   sizeList: ['small', '', 'large'] as const,
-// })
-
-// const { circleUrl, squareUrl, sizeList } = toRefs(state)
 </script>
 
 <style lang="less" scoped>
@@ -67,35 +96,66 @@ export default {
   background-color: #fff;
 }
 
-// 标签页中字体样式啥的
-.demo-tabs {
-  padding: 32px;
-  color: #3a67b6;
-  font-size: 32px;
-  font-weight: 600;
-}
-
 // 班级卡片整体
-.time {
-  font-size: 6px;
-  color: #999;
-}
-
-.bottom {
-  margin-top: 13px;
-  line-height: 12px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-// .button {
-//   padding: 0;
-//   min-height: auto;
-// }
-
-.image {
+.style_classListBox__NBMYd {
+  // background: #f5f5f5;
+  flex-direction: row;
+  height: 100vh;
+  left: 0;
+  padding-top: 10px;
+ 
+  // position: fixed;
+  top: 0;
   width: 100%;
-  display: block;
+}
+.style_listBox__40SsE {
+  height: 100%;
+  overflow: scroll;
+  width: 100%;
+}
+
+// 班级卡片 一整张
+.style_listBox__40SsE .style_item__BJ73k {
+  background: #fff;
+  border-radius: 16px;
+  box-shadow: 0 5px 10px 0 rgb(50 50 93 / 35%);
+  display: flex;
+  flex-direction: column;
+ 
+  float: left;
+  height: 140px;
+  margin-bottom: 5px;
+  margin-top: 4px;
+   margin-left: 10px;
+  padding: 20px 24px;
+  width: calc(43% - 12px);
+  // background-color: rgb(168, 21, 21);
+}
+
+// 班级卡片 上半部分
+.style_listBox__40SsE .style_item__BJ73k .style_top__09yUH {
+  border-bottom: 1px solid #f0f0f0;
+  cursor: pointer;
+  display: flex;
+  height: 80px;
+  width: 100%;
+  // background-color: rgb(168, 21, 21);
+}
+
+// 班级卡片  头像
+.style_listBox__40SsE .style_item__BJ73k .style_top__09yUH .style_left__l5taP {
+  height: 54px;
+  margin-right: 16px;
+  width: 54px;
+  // background-color: rgb(168, 21, 21);
+}
+.style_classListLeft__MZMoX .style_listBox__40SsE .style_item__BJ73k .style_btm__xPuHV {
+    align-items: center;
+    color: #356cfe;
+    display: flex;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 25px;
+    // margin-top: 20px;
 }
 </style>
